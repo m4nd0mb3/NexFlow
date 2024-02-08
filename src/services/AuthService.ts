674af215bcbeb098a96f8f08ac4ba2b3ab4eacc5
Jonathan Mandombe <jonathan.mandombe@ideiasdinamicas.com>
2024-02-08 15:34:18 +0100

    chore: update timeout to token validation user

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -6,6 +6,6 @@ dotenv.config();
 
 export const generateToken = (user: User): string => {
     return jwt.sign({ uuid: user.uuid, 'name': user.name, email: user.email }, process.env.JWT_SECRET as string, {
-        expiresIn: '1h', 
+        expiresIn: '24h', 
     });
 };
